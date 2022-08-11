0b1ca691b3221a3f1ca2af4c2d56e7320dfb3fc7
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-08-11 10:56:00 +0300

    CategoryDTJ external js olarak tekrar tasarlandı

--- Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
+++ Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.ENTITIES.Models;
 using Project.MVCUI.Areas.Admin.AdminVMClasses;
 using Project.MVCUI.AuthenticationClasses;
 using System;
@@ -27,6 +28,37 @@ namespace Project.MVCUI.Areas.Admin.Controllers
             } : new CategoryVM { Categories = _cRep.Where(x => x.ID == id) };
 
             return View(cvm);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            _cRep.Add(category);
+            return RedirectToAction("CategoryList");
+        }
+
+        public ActionResult UpdateCategory(int id)
+        {
+            CategoryVM cvm = new CategoryVM { Category = _cRep.Find(id) };
+            return View(cvm);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            _cRep.Update(category);
+            return RedirectToAction("CategoryList");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            _cRep.Delete(_cRep.Find(id));
+            return RedirectToAction("CategoryList");
         }
     }
 }
