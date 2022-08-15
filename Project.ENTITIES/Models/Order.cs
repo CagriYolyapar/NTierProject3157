51d6b4ec73efb335dbf6f669fbac0b9f77d3fe1a
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-08-15 14:13:55 +0300

    Projenin sipariş işlemleri tamamlandı

--- Project.ENTITIES/Models/Order.cs
+++ Project.ENTITIES/Models/Order.cs
@@ -9,6 +9,13 @@ namespace Project.ENTITIES.Models
     public class Order:BaseEntity
     {
         public string ShippedAddress { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        //Sipariş işlemlerinin icerisindeki bilgileri daha rahat yakalamak adına actıgımız property'lerdern bir tanesi de TotalPrice'dir... Yalnız burada cok dikkatli olmamız gerekir gerçekten size hız kazandıracak bir durum varsa bu bilgileri ek olarak buraya almamız gerekir...Aynı zamanda bu durum abartılmamalıdır... Yani ilgili yapının tüm verileri asla bu sınıfa komple koyulmamalıdır...Sadece spesifik parcalar alınmalıdır
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
         public int? AppUserID { get; set; }
         public int? ShipperID { get; set; }
 
